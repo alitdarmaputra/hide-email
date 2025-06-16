5b9c999acb5226e80b1fbb3c9e58c16fca78bfe1
alitdarmaputra <alitdarmaputra@gmail.com>
2025-06-16 23:56:45 +0800

    feat: add comment for readabilty

--- scripts/hide-email.js
+++ scripts/hide-email.js
@@ -1,18 +1,23 @@
 const observer = new MutationObserver(() => {
+    // get all tag that contain email
     const emailContainers = document.querySelectorAll(".go");
 
     for (const container of emailContainers) {
+        // get email node
         const email = container.childNodes[1];
 
         if (!email) continue;
 
+        // regex to get username in email address
         const usernamePattern = /^.(.*?)(?=@)/;
+
         const tokens = email.textContent.match(usernamePattern);
 
         if (!tokens) continue;
 
         let censoredUsn = '';
 
+        // censor the username
         for (let i = 0; i < tokens[0].length; i++) {
             if (i === 0 && tokens[0].length === 1) {
                 censoredUsn += '*';
@@ -23,6 +28,7 @@ const observer = new MutationObserver(() => {
             }
         }
 
+        // update website ui with the censored email address
         const censoredEmail = email.textContent.replace(usernamePattern, censoredUsn);
         email.textContent = censoredEmail;
     }
